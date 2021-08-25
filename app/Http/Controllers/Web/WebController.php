2b06ccdab1f6fb7aca327d6a8fa1f8b0482f44ec
Mateus Reis <mateuskonige@outlook.com>
2021-08-25 11:13:22 -0300

    website consumindo dados planos

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class WebController extends Controller
 {
     public function index() {
-        
-        return view('web.pages.index');
+        $plans = Plan::with('details')->get();
+
+        return view('web.pages.index', compact('plans'));
     }
 }
