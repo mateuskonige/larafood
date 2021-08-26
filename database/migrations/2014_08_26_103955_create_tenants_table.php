3f9918d22365798f6f4e9dda0a73532101bf394c
Mateus Reis <mateuskonige@outlook.com>
2021-08-26 11:04:27 -0300

    Model Migration Tenant

---
+++ database/migrations/2014_08_26_103955_create_tenants_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('plan_id');
+            $table->string('cpnj')->unique();
+            $table->string('name')->unique();
+            $table->string('url')->unique();
+            $table->string('email')->unique();
+            $table->string('logo')->nullable();
+
+            //Status tenant (Se inativar 'N' ele perde o acesso ao sistema)
+            $table->enum('active', ['Y', 'N'])->default('Y');
+
+            //Subscription
+            $table->date('subscription')->nullable(); //Data de inscrição
+            $table->date('expires_at')->nullable(); //Data que expira o acesso
+            $table->date('subscription_id', 255)->nullable(); //Identificador do Gateway
+            $table->boolean('subscription_active')->nullable(); //Assinatura ativa
+            $table->boolean('subscription_suspended')->nullable(); //Assinatura cancela
+
+            $table->foreign('plan_id')->references('id')->on('plans');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
