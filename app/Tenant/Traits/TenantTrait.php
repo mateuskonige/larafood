59f4cdbe3dabad8ded33e55cc2d87477cb5c6bfa
Mateus Reis <mateuskonige@outlook.com>
2021-09-08 07:32:16 -0300

    global TenantScope

--- app/Tenant/Traits/TenantTrait.php
+++ app/Tenant/Traits/TenantTrait.php
@@ -3,11 +3,13 @@
 namespace App\Tenant\Traits;
 
 use App\Tenant\Observers\TenantObserver;
+use App\Tenant\Scopes\TenantScope;
 
 trait TenantTrait {
     
     protected static function booted()
     {
         static::observe(TenantObserver::class);
+        static::addGlobalScope(new TenantScope);
     }
 }
